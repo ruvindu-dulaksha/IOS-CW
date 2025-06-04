5e474794984bacb6edaa35cc3a0125a3e0089aec
ruvindu-dulaksha <ruvindufdo@gmail.com>
2025-06-04 14:26:56 +0530

    Initial commit

---
+++ NibmUniBuddy/NibmUniBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NibmUniBuddyApp.swift
+//  NibmUniBuddy
+//
+//  Created by Ruvindu Dulaksha on 2025-06-04.
+//
+
+import SwiftUI
+
+@main
+struct NibmUniBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
